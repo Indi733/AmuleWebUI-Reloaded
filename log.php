aea0fd259a5f3dba63172225773981a15716c7eb
Indi733 <126485041+Indi733@users.noreply.github.com>
2025-11-20 19:57:03 +0100

    Refactor log.php for modern PHP and styling

--- log.php
+++ log.php
@@ -1,39 +1,52 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html><head>
-<title>amule log page</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="pragmas" content="no-cache">
-<?php
-	if ( $_SESSION["auto_refresh"] > 0 ) {
-		echo "<meta http-equiv=\"refresh\" content=\"", $_SESSION["auto_refresh"], '">';
-	}
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <title>aMule Log Component</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="Pragma" content="no-cache">
+    
+    <?php
+        if ( isset($_SESSION["auto_refresh"]) && $_SESSION["auto_refresh"] > 0 ) {
+            echo '<meta http-equiv="refresh" content="', $_SESSION["auto_refresh"], '">';
+        }
+    ?>
 
-?>
-<style type="text/css">
-<!--
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-	background-color: #FFFFFF;
-	font-size: small;
-	font-family: Fixed;
-	color: #000000;
-}
--->
-</style>
+    <style>
+        body { 
+            background-color: #000000; /* Terminal Black */
+            color: #e9ecef; 
+            margin: 0; 
+            padding: 10px;
+            font-family: 'Consolas', 'Monaco', 'Courier New', monospace;
+            font-size: 13px;
+        }
+        pre {
+            white-space: pre-wrap; 
+            word-wrap: break-word;
+            margin: 0;
+        }
+        /* Matrix Green for standard logs */
+        .log-text { color: #00ff00; }
+        /* Cyan for server logs */
+        .srv-text { color: #00d8ff; }
+    </style>
 </head>
 <body>
-<code><pre>
-<?php
-	if ("srv" == $HTTP_GET_VARS['show'] || 1 == $HTTP_GET_VARS['rstsrv']) {
-		echo amule_get_serverinfo($HTTP_GET_VARS['rstsrv']);
-	 }else {
-		echo amule_get_log($HTTP_GET_VARS['rstlog']);
-	}
-?>
-</pre></code>
+    <?php
+        // Modern PHP $_GET replacement
+        $show = isset($_GET['show']) ? $_GET['show'] : '';
+        $rstsrv = isset($_GET['rstsrv']) ? $_GET['rstsrv'] : 0;
+        $rstlog = isset($_GET['rstlog']) ? $_GET['rstlog'] : 0;
+
+        // Determine content type
+        if ($show == "srv" || $rstsrv == 1) {
+            $content = amule_get_serverinfo($rstsrv);
+            $class = "srv-text";
+        } else {
+            $content = amule_get_log($rstlog);
+            $class = "log-text";
+        }
+    ?>
+    <pre class="<?php echo $class; ?>"><?php echo htmlspecialchars($content); ?></pre>
 </body>
 </html>
