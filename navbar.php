5f99893fb71dcc60e49113131f89fc6f9caa6701
Indi733 <126485041+Indi733@users.noreply.github.com>
2025-11-20 16:16:13 +0100

    Add responsive navigation bar to navbar.php

---
+++ navbar.php
@@ -0,0 +1,62 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top border-bottom border-secondary">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <img src="logo-nav-brax.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
+            aMule WebUI
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-dload.php">
+                        <i class="bi bi-arrow-down-up d-block fs-5"></i> Transfer
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-shared.php">
+                        <i class="bi bi-share d-block fs-5"></i> Shared
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-search.php">
+                        <i class="bi bi-search d-block fs-5"></i> Search
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-servers.php">
+                        <i class="bi bi-hdd-network d-block fs-5"></i> Servers
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-kad.php">
+                        <i class="bi bi-diagram-3 d-block fs-5"></i> Kad
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-stats.php">
+                        <i class="bi bi-bar-chart d-block fs-5"></i> Stats
+                    </a>
+                </li>
+                <li class="nav-item border-start border-secondary mx-2"></li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-prefs.php">
+                        <i class="bi bi-gear d-block fs-5"></i> Settings
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link" href="amuleweb-main-log.php">
+                        <i class="bi bi-journal-text d-block fs-5"></i> Logs
+                    </a>
+                </li>
+                <li class="nav-item text-center mx-2">
+                    <a class="nav-link text-danger" href="login.php">
+                        <i class="bi bi-power d-block fs-5"></i> Exit
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
