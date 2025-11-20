ba4d9fe10bc726a081d24d964c86c958b6db68e8
Indi733 <126485041+Indi733@users.noreply.github.com>
2025-11-20 16:12:08 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,119 +1,67 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
 <head>
-	<title>aMule - Control Panel - Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aMule - Login</title>
 
-	<script language="JavaScript" type="text/javascript">
-		function login_init()
-		{
-			breakout_of_frame();
-			document.login.pass.focus();
-		}
-	</script>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" integrity="sha384-7tY7Dc2Q8WQTKGz2Fa0vC4dWQo07N4mJjKvHfIGnxuC4vPqFGFQppd9b3NWpf18/" crossorigin="anonymous">
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css" integrity="sha384-BD3p+z3TqIhBK2OaMBRzK4Nz02t4OQcwrEkJxy3PAqU2Rwm1giS6RCgvBDk6+iPH" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js" integrity="sha384-oFMgcGzKX7GaHtF4hx14KbxdsGjyfHK6m1comHjI1FH6g4m6qYre+4cnZbwaYbHD" crossorigin="anonymous"></script>
+    <style>
+        body, html {
+            height: 100%;
+            background-color: #212529;
+        }
+        .login-container {
+            max-width: 400px;
+            width: 100%;
+        }
+        .logo-img {
+            width: 150px;
+            margin-bottom: 20px;
+        }
+        .brand-color {
+            color: #4db6ac;
+        }
+    </style>
 
-	<style type="text/css">
-		body {
- 			padding-top: 50px;
- 			background-color:#39425f;
-		}
-		.logo-cont {
-  			padding: 40px 15px;
-  			text-align: center;
-		}
-		.logo-img {
-			border: 3px solid #BDBDBD;
-			border-radius: 30px;
-		}
-		.logo-nav {
-			height: 40px;
-			width: 40px;
-		}
-		.navbar-brand {
-			padding-top: 5px;
-		}
-		.navbar-form input[type="password"] {
-			height: 28px;
-		}
-	</style>
-
-		 <!-- /* Styling for Brax AmuleWebUI Material Theme */-->
-        <style text="css/text">
-
-                .navbar {
-                background-color:#2f303d;
-                }
-                .label-success {
-                        background-color:#319a9b;
-                }
-                .label-default {
-                        background-color:#ffffff;
-                        color:#319a9b;
-                }
-                .panel {
-                        background-color:#39425f;
-                        border: 0;
-                }
-                .panel-heading{
-                        background-color:#319a9b;
-                        border: 0;
-                }
-                .form-control {
-                border: 0;
-                }
-                .table > thead > tr > th, .table > thead > tr > td {
-                        border: 0;
-                }
-                .glyphicon {
-                        color:#319a9b;
-                }
-                .btn:hover .glyphicon{
-			color:#fff;
-                  }
-                  a:hover {
-                        color:#fff;
-                        }
-                a {
-                        color:#4db6ac;
-                }
-                h4 {
-                        color:#cfd8dc;
-                }
-                 td {
-                        color:#cfd8dc
-                }
-                  th {
-                        color:#4db6ac
-                }
-        </style>
-
+    <script>
+        function login_init() {
+            if (top.location != self.location) {
+                top.location = self.location.href;
+            }
+            document.getElementById('pass').focus();
+        }
+    </script>
 </head>
 
-<body class="animated fadeIn" style="animation-duration: 1.5s" onload="login_init();">
+<body class="d-flex align-items-center justify-content-center animated fadeIn" onload="login_init();">
 
-    <div class="navbar navbar-fixed-top" "role="navigation">
-	    <div class="container">
-	    	<a class="navbar-brand" href="#"><img src="logo-nav-brax.png" class="logo-nav"> aMule WebUI</a>
-	    	<form class="navbar-form navbar-right" role="form" name="login">
-				<div class="form-group">
-			   		<input name="pass" type="password" class="form-control" placeholder="Password" required autofocus>
-			   		<button class="btn btn-primary" type="submit" name="submit" value="Submit">
-			   			<span class="glyphicon glyphicon-log-in" style="color:#ffffff"></span>
-			   		</button>
-		    	</div>
-    		</form>
-    		</div>
-    	</div>
+    <div class="login-container text-center">
+        <div class="card shadow-lg border-0">
+            <div class="card-body p-5">
+                <img src="logo-brax.png" class="logo-img animated rubberBand" alt="aMule Logo">
+                <h2 class="mb-4 brand-color">aMule WebUI</h2>
+                <p class="text-muted mb-4">Please login to access the interface</p>
+
+                <form name="login" method="post" action="login.php">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text"><i class="bi bi-key"></i></span>
+                        <div class="form-floating flex-grow-1">
+                            <input type="password" name="pass" id="pass" class="form-control" placeholder="Password" required>
+                            <label for="pass">Password</label>
+                        </div>
+                        <button class="btn btn-primary" type="submit" name="submit" value="Submit">
+                            <i class="bi bi-box-arrow-in-right"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="container logo-cont">
-    	<img class="animated rubberBand" style="animation-duration: 1s" src="logo-brax.png" width="400" height="400">
-    	<h1 style="color:#319a9b;">aMule Web Interface</h1>
-    	<p style="color:white;">Welcome!<br>Please login to access the complete interface!</p>
-    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
